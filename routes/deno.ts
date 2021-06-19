a52a0b6a590bbcd1dcee544a9b530ffa55dc5442
pai <hachikopolarcito321@gmail.com>
2021-06-19 01:25:54 -0500

    just left highlight syntax

--- routes/deno.ts
+++ routes/deno.ts
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const { exec } = require("child_process");
 const router = Router();
 const fs = require("fs");
-const markdown = require("markdown").markdown;
 
 router.post("/code", (req, res) => {
   const { code } = req.body;
@@ -16,20 +15,16 @@ router.post("/code", (req, res) => {
   });
   exec(`./deno run -A --no-check  execute.ts`, (error, stdout, stderr) => {
     console.log(stdout);
-    let out = markdown
-      .toHTML(stdout || "")
-      .replace(
-        /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
-        ""
-      );
-    let err = markdown
-      .toHTML(stderr || "")
-      .replace(
-        /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
-        ""
-      );
+    let out = stdout.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+      ""
+    );
+    let err = stderr.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+      ""
+    );
     res.write(
-      `<div align="center"><h1>your output code:</h1> <p>${out}</p> <h1>possibly error:</h1> ${err} </div>`
+      `<h1 align="center">your output code:</h1> <xmp>${out}</xmp> <h1 align="center">possibly error:</h1> <xmp>${err}</xmp>`
     );
   });
 });
